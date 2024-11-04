eb51a936f3fe5ca5ec05db70c5bc5434e0c0231c
Jhonatan <jhonatan_bellaiza@hotmail.com>
2024-11-04 17:39:39 -0600

    Upload project

---
+++ src/main/java/edu/miu/sdamidetermprepartion/SdaMidetermPrepartionApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.sdamidetermprepartion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaMidetermPrepartionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaMidetermPrepartionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/sdamidetermprepartion/SdaMidetermPrepartionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.sdamidetermprepartion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaMidetermPrepartionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
